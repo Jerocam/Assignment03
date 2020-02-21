d70f3bc724c3a9fefe2beb2093a731ecd7994848
Jeronimo Jose Ocampos <ocamposjero@gmail.com>
2020-02-20 20:54:56 -0800

    Add files via upload

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,9 +1,58 @@
 //STEP 1
+/*let number= parseInt(window.prompt("Enter a number for half of this number"));
+let result = halfNumber(number);
+
+function halfNumber(half) {
+return half/2;
+}
+
+document.write("Half of "+number+ " is "+result);
 
 //STEP 2
+let number= parseInt(window.prompt("Enter a number for square of that number"));
+let result = squareNumber(number);
+
+function squareNumber(square) {
+return square*square;
+}
+
+document.write("The result of squaring the number "+number+ " is "+result);
 
 //STEP 3
+let number= parseInt(window.prompt("Enter a number for percent"));
+let number2= parseInt(window.prompt("Enter a second number for percent"));
+let result = percentOf(number, number2);
+
+function percentOf(percent, percent2) {
+return percent/percent2*100;
+}
+
+document.write(+number+" is" +result+ "% of "+number2);
 
 //STEP 4
+let number= parseInt(window.prompt("Enter a number for percent"));
+let number2= parseInt(window.prompt("Enter a second number for percent"));
+let result = findModulus(number, number2);
+
+function findModulus(modulus, modulus2) {
+return modulus%modulus2;
+}
+
+document.write(+result+" is the modulus of " +number+ " and "+number2);*/
 
 //STEP 5
+let number= parseInt(window.prompt("Enter a number"));
+let number2= parseInt(window.prompt("Enter a second number"));
+let number3= parseInt(window.prompt("Enter a third number"));
+
+let result = sumTotal(number, number2, number3);
+
+function sumTotal(sum1, sum2, sum3) {
+    let total=0;
+    for (let i = 0; i <arguments.length; i++){
+        total += arguments[i];
+        
+    }return total;
+}
+
+document.write("The total of sums are " +result);
---
+++ js/02-fortune-teller.js
@@ -0,0 +1,12 @@
+/*let numberChildren = parseInt(window.prompt("How many children do you have?"));
+let namePartner = window.prompt("What is your partner's name?");
+let location = window.prompt("where do you live?");*/
+let jobTitle = tellFortune(window.prompt("What is your job title?")) ;
+
+
+function tellFortune(job){
+
+    document.write("You will be a"+job +"in Y, and married to Z with N kids.");
+
+}
+
