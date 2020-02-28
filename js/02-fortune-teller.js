dd5fb9f88b3453202eb3170dc44fbc0103ce5ef8
Jeronimo Jose Ocampos <ocamposjero@gmail.com>
2020-02-27 18:26:56 -0800

    Update 02-fortune-teller.js

--- js/02-fortune-teller.js
+++ js/02-fortune-teller.js
@@ -1,9 +1,10 @@
-
+//FUNCTION CALL TELLFORTUNE WITH 4 PARAMETERS
 function tellFortune(children,partnerName,gplace,jobTitle){
     
-    document.write("You will be a "+jobTitle+" in "+gplace+", and married to "+partnerName+" with " +children+" kids.<br>");
+    document.write("You will be a "+jobTitle+" in "+gplace+", and married to "+partnerName+" with " +children+" kids.<br>"); //it displays with the results from values
 }
 
+//VALUES TO CALL FUNCTION TELLFORTUNE
 tellFortune (3,"Maria","San Diego","Graphic Designer");
 tellFortune (9,"Jorge","Dallas","Lawyer");
 tellFortune (2,"Pablo","Miami City","Mechanic");
