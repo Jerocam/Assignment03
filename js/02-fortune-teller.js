1e488a20a10c58595aacfcbfdeebe2fe3e6fb8e3
Jeronimo Jose Ocampos <ocamposjero@gmail.com>
2020-02-27 18:52:34 -0800

    Update 02-fortune-teller.js

--- js/02-fortune-teller.js
+++ js/02-fortune-teller.js
@@ -1,6 +1,8 @@
+/*eslint-env browser*/
+
 //FUNCTION CALL TELLFORTUNE WITH 4 PARAMETERS
 function tellFortune(children,partnerName,gplace,jobTitle){
-    
+    "use strict";
     document.write("You will be a "+jobTitle+" in "+gplace+", and married to "+partnerName+" with " +children+" kids.<br>"); //it displays with the results from values
 }
 
